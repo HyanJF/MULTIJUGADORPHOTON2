cf325e2f35dfbc31ed79cd497c3b8630aa87c242
HF <hyan.fierros@uniat.edu.mx>
2025-09-04 15:30:22 -0600

    first commit

---
+++ Assets/PlayerSpawner/PlayerSpawner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Fusion;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject playerPrefab;
+    public void PlayerJoined(PlayerRef player)
+    {
+        //Runner tiene el componente similar al local player
+        if (player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(playerPrefab, new Vector3(0,1,0), Quaternion.identity);
+        }
+    }
+
+}
